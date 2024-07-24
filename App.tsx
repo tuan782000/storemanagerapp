ca7c892a96b17fa4d18c955a3991761bcc22ce4b
tuannguyen <thaituan7820@gmail.com>
2024-07-24 09:36:33 +0700

    storemanageapp

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
