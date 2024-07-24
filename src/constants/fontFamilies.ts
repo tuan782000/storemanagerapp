7f716e3846759a3a2f69d00bd6d6ab54b8cc9648
tuannguyen <thaituan7820@gmail.com>
2024-07-24 09:44:43 +0700

    config react native vector icon - icon sax and fonts

---
+++ src/constants/fontFamilies.ts
@@ -0,0 +1,7 @@
+export const fontFamilies = {
+  regular: 'Montserrat-Regular',
+  medium: 'Montserrat-Medium',
+  bold: 'Montserrat-Bold',
+  thin: 'Montserrat-Thin',
+  light: 'Montserrat-Light',
+};
