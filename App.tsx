c7eb6e2b20406b69f055b1557825a22dc890f079
tuannguyen <thaituan7820@gmail.com>
2024-07-24 10:00:24 +0700

    fix upload react vector icon to IOS and Android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {fontFamilies} from './src/constants/fontFamilies';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>App</Text>
+      <Text style={{fontFamily: fontFamilies.regular, fontSize: 20}}>App</Text>
     </View>
   );
 };
