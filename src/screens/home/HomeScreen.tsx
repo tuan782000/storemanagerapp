3bfa3a7cc59b4bd41c656f92e87bd19a73c5c0f0
tuannguyen <thaituan7820@gmail.com>
2024-07-27 10:45:37 +0700

    update chart

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -11,12 +11,50 @@ import {BarChart, LineChart, PieChart} from 'react-native-gifted-charts';
 import LinearGradient from 'react-native-linear-gradient';
 
 const barChartData: any = [
-  {value: 50, label: 'Jan'},
-  {value: 80, label: 'Feb'},
-  {value: 90, label: 'Mar'},
-  {value: 70, label: 'Apr'},
-  {value: 85, label: 'May'},
-  {value: 60, label: 'Jun'},
+  {
+    value: 2500,
+    frontColor: '#006DFF',
+    gradientColor: '#009FFF',
+    spacing: 6,
+    label: 'Jan',
+  },
+  {value: 2400, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+  {
+    value: 3500,
+    frontColor: '#006DFF',
+    gradientColor: '#009FFF',
+    spacing: 6,
+    label: 'Feb',
+  },
+  {value: 3000, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+  {
+    value: 4500,
+    frontColor: '#006DFF',
+    gradientColor: '#009FFF',
+    spacing: 6,
+    label: 'Mar',
+  },
+  {value: 4000, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+  {
+    value: 5200,
+    frontColor: '#006DFF',
+    gradientColor: '#009FFF',
+    spacing: 6,
+    label: 'Apr',
+  },
+  {value: 4900, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+  {
+    value: 3000,
+    frontColor: '#006DFF',
+    gradientColor: '#009FFF',
+    spacing: 6,
+    label: 'May',
+  },
+  {value: 2800, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
 ];
 
 const lineChartData: any = [
@@ -82,7 +120,7 @@ const HomeScreen = ({navigation}: any) => {
       </View>
 
       <View style={[globalStyles.container, globalStyles.center]}>
-        <BarChart
+        {/* <BarChart
           data={barChartData}
           barWidth={30}
           barBorderRadius={5}
@@ -92,9 +130,49 @@ const HomeScreen = ({navigation}: any) => {
           isAnimated
           animationDuration={1000}
           // animationType="SlideFromBottom"
-        />
+        /> */}
+        <View
+          style={{
+            margin: 10,
+            padding: 16,
+            borderRadius: 20,
+            backgroundColor: '#232B5D',
+          }}>
+          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+            Overview
+          </Text>
+          <View style={{padding: 20, alignItems: 'center'}}>
+            <BarChart
+              data={barChartData}
+              barWidth={16}
+              initialSpacing={10}
+              spacing={14}
+              barBorderRadius={4}
+              showGradient
+              yAxisThickness={0}
+              xAxisType={'dashed'}
+              xAxisColor={'lightgray'}
+              yAxisTextStyle={{color: 'lightgray'}}
+              stepValue={1000}
+              maxValue={6000}
+              noOfSections={6}
+              yAxisLabelTexts={['0', '1k', '2k', '3k', '4k', '5k', '6k']}
+              labelWidth={40}
+              xAxisLabelTextStyle={{color: 'lightgray', textAlign: 'center'}}
+              showLine
+              lineConfig={{
+                color: '#F29C6E',
+                thickness: 3,
+                curved: true,
+                hideDataPoints: true,
+                shiftY: 20,
+                initialSpacing: -30,
+              }}
+            />
+          </View>
+        </View>
       </View>
-      <View style={[globalStyles.container, globalStyles.center]}>
+      {/* <View style={[globalStyles.container, globalStyles.center]}>
         <LineChart
           data={lineChartData}
           thickness={2}
@@ -111,7 +189,7 @@ const HomeScreen = ({navigation}: any) => {
           animationDuration={1000}
         />
         <LinearGradient colors={['#ffa726', '#fb8c00']} style={{flex: 1}} />
-      </View>
+      </View> */}
 
       <View style={[globalStyles.container, globalStyles.center]}>
         <PieChart
